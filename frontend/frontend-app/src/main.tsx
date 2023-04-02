9f8637815047aefbedf583893ee412342354b880
921-Beltechi-Lois <lois.beltechi@stud.ubbcluj.ro>
2023-04-03 00:37:45 +0300

    new_link

--- frontend/frontend-app/src/main.tsx
+++ frontend/frontend-app/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-export const GlobalURL=`http://13.53.243.237`;
+export const GlobalURL=`http://13.51.250.171`;
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
